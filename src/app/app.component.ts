46440761a05b4e1b839878ba1f72f8b8f34280c6
Kyle Walker <kyl3walker@gmail.com>
2022-10-15 16:34:44 -0400

    updated to project to match prototype layout - setup theme for navigation bar and menu - added routing - added quiz and submission pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { shareReplay, map } from 'rxjs/operators';
+import { User } from './core/user';
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel
+} from '@angular/router';
+import { NotificationService } from './shared/notification.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hci-quiz-app';
+export class AppComponent implements OnInit {
+  loading: boolean = false;
+  loaded: boolean = false;
+  title = 'SLAAK Quiz';
+  user!: User;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private notificationService: NotificationService,
+    private router: Router
+  ) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
+
+  ngOnInit() {
+    this.loaded = false;
+    this.loading = true;
+    this.notificationService.openSnackBar('this is a test notification on start up.')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,28 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemWebApiService } from './core/in-mem-web-api.service';
+import { QuizComponent } from './quiz/quiz.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { CoreModule } from './core/core.module';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemWebApiService, {
+      delay: 1000
+    }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
@@ -22,7 +36,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     }),
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { floatLabel: 'always' } }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
