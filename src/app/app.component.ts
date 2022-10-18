8b19b76d24346f088e262910e4422a853e3075c1
Kyle Walker <kyl3walker@gmail.com>
2022-10-17 20:23:23 -0400

    added user service call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import {
   NavigationCancel
 } from '@angular/router';
 import { NotificationService } from './shared/notification.service';
+import { UserService } from './core/user.service';
 
 @Component({
   selector: 'app-root',
@@ -33,7 +34,8 @@ export class AppComponent implements OnInit {
   constructor(
     private breakpointObserver: BreakpointObserver,
     private notificationService: NotificationService,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) {
     router.events.subscribe((routerEvent: Event) => {
       this.checkRouterEvent(routerEvent);
@@ -57,6 +59,24 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.loaded = false;
     this.loading = true;
+        this.user = {
+      id: 0,
+      userid: ''
+    };
+    
+    this.userService.getUser().subscribe({
+      next: (data: User) => {
+        this.router.navigate(['quiz']);
+        this.loaded = true;
+      },
+      error: err => {
+        console.log(err);
+        this.user.userid = '';
+        this.router.navigate(['error']);
+        this.loaded = true;
+      }
+    });
+
     this.notificationService.openSnackBar('this is a test notification on start up.')
   }
 }
